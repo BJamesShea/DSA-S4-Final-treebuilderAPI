dd4be319dbd4527c880f8ed92de11641fbc48538
Brandon Shea <brandon.shea1994@icloud.com>
2025-04-02 14:14:39 -0230

    Initial commit

---
+++ src/main/java/com/keyin/treebuilderAPI/TreebuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.treebuilderAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreebuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreebuilderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/treebuilderAPI/TreebuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.treebuilderAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreebuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
