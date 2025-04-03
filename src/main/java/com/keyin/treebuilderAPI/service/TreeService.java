d09bb99b483af5c922333cd5c835a9106a1730cf
Brandon Shea <brandon.shea1994@icloud.com>
2025-04-03 19:08:33 -0230

    Scaffolding of TreeService

--- src/main/java/com/keyin/treebuilderAPI/service/TreeService.java
+++ src/main/java/com/keyin/treebuilderAPI/service/TreeService.java
@@ -1,4 +1,20 @@
 package com.keyin.treebuilderAPI.service;
 
+import com.keyin.treebuilderAPI.model.TreeNode;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class TreeService {
+
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
 }
