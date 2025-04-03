2d83cd605e7713fff998231db5c012d954b20aea
Brandon Shea <brandon.shea1994@icloud.com>
2025-04-03 18:58:42 -0230

    TreeNode constructors + getters / setters

--- src/main/java/com/keyin/treebuilderAPI/model/TreeNode.java
+++ src/main/java/com/keyin/treebuilderAPI/model/TreeNode.java
@@ -1,4 +1,38 @@
 package com.keyin.treebuilderAPI.model;
 
+@SuppressWarnings("ALL") // supress Lombok. Wanting to learn more about Java fundamentals so purposefully didn't import Lomnbok.
 public class TreeNode {
+
+    private String value;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
 }
