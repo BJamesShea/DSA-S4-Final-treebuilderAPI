395b16d170ba00267110053d65e4dc2c301c375b
Brandon Shea <brandon.shea1994@icloud.com>
2025-04-04 14:42:19 -0230

    TreeController implementation

--- src/main/java/com/keyin/treebuilderAPI/controller/TreeController.java
+++ src/main/java/com/keyin/treebuilderAPI/controller/TreeController.java
@@ -1,4 +1,30 @@
 package com.keyin.treebuilderAPI.controller;
 
+import com.keyin.treebuilderAPI.model.TreeNode;
+import com.keyin.treebuilderAPI.service.TreeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/tree")
 public class TreeController {
+
+    @Autowired
+    private TreeService treeService;
+
+    @PostMapping
+    public String setTree(@RequestBody TreeNode root) {
+        treeService.setRoot(root);
+        return "Tree has been set.";
+    }
+
+    @GetMapping
+    public TreeNode getTree() {
+        return treeService.getRoot();
+    }
+
+
+
+
 }
