a00516a150a58e796ff242011be241c6fc23f3fc
Brandon Shea <brandon.shea1994@icloud.com>
2025-04-18 14:51:21 -0230

    TreeRequestDTO scaffolding

--- src/main/java/com/keyin/treebuilderAPI/model/TreeRequestDTO.java
+++ src/main/java/com/keyin/treebuilderAPI/model/TreeRequestDTO.java
@@ -3,4 +3,13 @@ package com.keyin.treebuilderAPI.model;
 import java.util.List;
 
 public class TreeRequestDTO {
+    private List<Integer> values;
+
+    public List<Integer> getValues() {
+        return values;
+    }
+
+    public void setValues(List<Integer> values) {
+        this.values = values;
+    }
 }
