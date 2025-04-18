0a6f6f8792db011a4754eded5e8a2a314b822ccf
Brandon Shea <brandon.shea1994@icloud.com>
2025-04-18 15:55:05 -0230

    TreeNodeEntity boilerplate

--- src/main/java/com/keyin/treebuilderAPI/model/TreeNodeEntity.java
+++ src/main/java/com/keyin/treebuilderAPI/model/TreeNodeEntity.java
@@ -1,6 +1,54 @@
 package com.keyin.treebuilderAPI.model;
 
+import jakarta.persistence.*;
 
-
+@Entity
+@Table(name = "tree_nodes")
 public class TreeNodeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String value;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "left_id")
+    private TreeNodeEntity left;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "right_id")
+    private TreeNodeEntity right;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public TreeNodeEntity getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNodeEntity left) {
+        this.left = left;
+    }
+
+    public TreeNodeEntity getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNodeEntity right) {
+        this.right = right;
+    }
 }
